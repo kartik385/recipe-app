d482cd7ac2e974e040fced3274fb586f4bd82f54
kartik385 <kartikb385@gmail.com>
2021-05-21 17:31:35 +0530

    added more recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -3,7 +3,7 @@ import { EventEmitter, Injectable } from '@angular/core';
 import { Ingrediant } from "../shared/ingrediant.model";
 @Injectable()
 export class RecipeService{
-    recipes:Recipe[]=[new Recipe("pizza","Italian","https://imgmedia.lbb.in/media/2019/01/5c2f84d1d5b20859433ee25e_1546618065799.jpg",[new Ingrediant("tomato",5),new Ingrediant("bread",1)]),new Recipe("pizza","Italian","https://imgmedia.lbb.in/media/2019/01/5c2f84d1d5b20859433ee25e_1546618065799.jpg",[new Ingrediant("tomato",5),new Ingrediant("bread",1)])];
+    recipes:Recipe[]=[new Recipe("Pizza","Italian cousine","https://imgmedia.lbb.in/media/2019/01/5c2f84d1d5b20859433ee25e_1546618065799.jpg",[new Ingrediant("Tomato",5),new Ingrediant("Bread",1)]),new Recipe("Roast Butternut coup","Swiss cousine","https://cookieandkate.com/images/2019/10/best-butternut-squash-soup-recipe-2-768x1154.jpg",[new Ingrediant("Butter",1),new Ingrediant("Garlic",3),new Ingrediant("Shallot",2)]),new Recipe("Rattatouile","Italian cousine","https://cookieandkate.com/images/2019/09/best-ratatouille-recipe-1-1-768x1154.jpg",[new Ingrediant("zucchini",2),new Ingrediant("basil",9)])];
     clickedRecipe=new EventEmitter<Recipe>();
     addToShopping=new EventEmitter<Ingrediant[]>();
     getRecipies(){
